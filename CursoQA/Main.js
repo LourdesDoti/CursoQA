8481cb1e27c46f14c0bbc0335f5357f3c210ba32
Francisco <franciscodotitexeira@gmail.com>
2019-10-02 12:42:33 -0300

    Primer commit a este branch2

--- CursoQA/Main.js
+++ CursoQA/Main.js
@@ -1,6 +1,7 @@
-console.log ("Hola Mundo");
-console.log ("Hola mamasita");
-console.log (" blbalbalbal");
+console.log("Hola Mundo");
+console.log("Hola mamasita");
+console.log(" blbalbalbal");
 console.log("sos de la b");
 console.log("river gil");
 console.log("positivo")
+console.log("Fran agrego texto0");
